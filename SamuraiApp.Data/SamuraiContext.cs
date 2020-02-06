ba571b78941d8d2c03b8442996d4de40dbb6aa32
Seth NeJame <seth@worldfavor.com>
2020-02-06 09:14:10 +0100

    Program.cs @ ConsoleApp now runs a quick operation w/ EFCore to add/fetch samurais

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -11,5 +11,14 @@ namespace SamuraiApp.Data
     public class SamuraiContext: DbContext
     {
         public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Clan> Clans { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData");
+        }
+
     }
 }
