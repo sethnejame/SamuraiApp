7ce98189c1774daca5a48a38fdee8b81b1f2a304
Seth NeJame <seth@worldfavor.com>
2020-02-06 09:00:57 +0100

    Setting up SamuraiContext and dbContext configuration

---
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext: DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+    }
+}
