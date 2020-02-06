b82b4d098e8ce10df5160a4d3075d2c2d2765d6c
Seth NeJame <seth@worldfavor.com>
2020-02-06 08:41:24 +0100

    Adding initial SamuraiApp.Domain classes

---
+++ SamuraiApp.Domain/Clan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamuraiApp.Domain
+{
+    public class Clan
+    {
+        public int Id { get; set; }
+        public string ClanName { get; set; }
+
+    }
+}
