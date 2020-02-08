4da2b5e3ac44d40cfb0335fe42c737b962ae2d58
Seth <sethnejame@gmail.com>
2020-02-08 18:04:07 +0100

    Adding logger to SamuraiContext to see DB ops

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using SamuraiApp.Domain;
 using System;
 using System.Collections.Generic;
@@ -10,15 +11,28 @@ namespace SamuraiApp.Data
 {
     public class SamuraiContext: DbContext
     {
+        // Each DbSet is a repository for objects of a particular type
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Clan> Clans { get; set; }
         public DbSet<Battle> Battles { get; set; }
 
+        public static readonly ILoggerFactory ConsoleLoggerFactory
+            = LoggerFactory.Create(builder =>
+            {
+                builder
+                .AddFilter((category, level) =>
+                    category == DbLoggerCategory.Database.Command.Name
+                    && level == LogLevel.Information)
+                .AddConsole();
+            });
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(
-                "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData");
+            optionsBuilder
+                .UseLoggerFactory(ConsoleLoggerFactory)
+                    .UseSqlServer(
+                    "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData");
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
